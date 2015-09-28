84a47e4a3d81ff98df48dfff40de6aee81b6598b
Richard Sparks <rsparks@zips-mbp-47.imkennedy.com>
2015-09-28 19:25:14 -0400

    Parsed data extracted

--- OpenAPI.js
+++ OpenAPI.js
@@ -1,11 +1,46 @@
+
+function Link(author, num_comments, over_18, permalink, score, subreddit, thumbnail, title, url){
+	this.author = author;
+	this.num_comments = num_comments;
+	this.over_18 = over_18;
+	this.permalink = permalink;
+	this.score = score;
+	this.subreddit = subreddit;
+	this.thumbnail = thumbnail;
+	this.title = title;
+	this.url = url;
+}
+
+var redditApp = new Object();
+redditApp.linkManager = [];
+
+redditApp.buildObjects = function(){
+	var redditData = parsedRequest.data.children
+	var myLink;
+	for (x in redditData){
+		myLink = new Link(
+			redditData[x].data.author,
+			redditData[x].data.num_comments,
+			redditData[x].data.over_18,
+			redditData[x].data.permalink,
+			redditData[x].data.score,
+			redditData[x].data.subreddit,
+			redditData[x].data.thumbnail,
+			redditData[x].data.title,
+			redditData[x].data.url
+			);
+		redditApp.linkManager.push(myLink);
+	}
+}
+
 var request = new XMLHttpRequest();
 var parsedRequest;
 request.open('GET', 'http://www.reddit.com/.json');
 request.onreadystatechange = function(){
 	if((request.status==200) && (request.readyState==4)){
 		parsedRequest = JSON.parse(request.responseText);
-		console.log(parsedRequest);
-		
+		redditApp.buildObjects();
+		console.log(redditApp.linkManager);
 	}
 }
 request.send();
