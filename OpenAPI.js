76247718899533fa716c20ead833c24ad58868db
Richard Sparks <rsparks@zips-mbp-47.imkennedy.com>
2015-09-29 22:51:15 -0400

    pictures showing, format might not be right

--- OpenAPI.js
+++ OpenAPI.js
@@ -1,4 +1,6 @@
 
+
+
 function Link(author, num_comments, over_18, permalink, score, subreddit, thumbnail, title, url){
 	this.author = author;
 	this.num_comments = num_comments;
@@ -33,6 +35,45 @@ redditApp.buildObjects = function(){
 	}
 }
 
+redditApp.writeToScreen = function(){
+	var comments = "";
+	var info = "";
+	var pic = "";
+	var title = "";
+	for (x in this.linkManager){
+		
+		
+		title += "<ul style=\"list-style-type:none\"><li><img src="+this.linkManager[x].thumbnail+" alt=\"gah?\" height=\"100\" width=\"100\">"+"</li>";
+
+		title += "<li><a href = "+this.linkManager[x].url;+">"+this.linkManager[x].title+"</a></li>";
+
+		title += "<li>"+this.linkManager[x].score+", ";
+		title += this.linkManager[x].subreddit+", ";
+		title += this.linkManager[x].author+",";
+		title += this.linkManager[x].over_18+"</li>";
+
+		title += "<li><a href = "+this.linkManager[x].num_comments+">"+this.linkManager[x].permalink+"</a></li><ul style=\"list-style-type:none\">";
+		
+
+
+	}
+		document.getElementById('title').innerHTML += title;
+
+	/**
+	document.getElementById('title').innerHTML += info;
+	document.getElementById('title').innerHTML += pic;
+	document.getElementById('title').innerHTML += comments;
+
+	document.getElementById('comments').innerHTML += comments;
+	document.getElementById('info').innerHTML += info;
+	document.getElementById('pic').innerHTML += pic;
+	document.getElementById('title').innerHTML += title;
+	*/
+}
+
+
+
+
 var request = new XMLHttpRequest();
 var parsedRequest;
 request.open('GET', 'http://www.reddit.com/.json');
@@ -40,6 +81,7 @@ request.onreadystatechange = function(){
 	if((request.status==200) && (request.readyState==4)){
 		parsedRequest = JSON.parse(request.responseText);
 		redditApp.buildObjects();
+		redditApp.writeToScreen();
 		console.log(redditApp.linkManager);
 	}
 }
